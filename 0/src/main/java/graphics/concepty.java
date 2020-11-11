896a5041772a2cdd4c75c7bc487a49d7d1a22f18
Phlogiston1 <sdbazoes@gmail.com>
2020-11-11 14:58:00 -0500

    remove unused imports

--- 0/src/main/java/graphics/concepty.java
+++ 0/src/main/java/graphics/concepty.java
@@ -1,45 +1,13 @@
 package graphics;
 
 import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.swing.JButton;
 import java.awt.GridLayout;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 
 public class concepty {
-    static ArrayList<JComponent> components = new ArrayList<>();
-    public static void getElements(XMLUtils xmlReader, Element root, ArrayList<JComponent> components, JPanel panel){
-
-        NodeList children = xmlReader.getChildren(root);
-        for(int i = 0; i < children.getLength(); i++){
-            if(children.item(i).getNodeName() == "button"){
-                components.add(new JButton(children.item(i).getTextContent()));
-            }
-            if(children.item(i).getNodeName() == "label"){
-                components.add(new JLabel(children.item(i).getTextContent()));
-            }
-            if(children.item(i).getNodeName() == "panel"){
-                JPanel newPanel = new JPanel();
-                getElements(xmlReader, (Element)children.item(i), new ArrayList<>(), newPanel);
-                components.add(newPanel);
-            }
-            
-        }
-        for(int i = 0; i < components.size(); i++){
-            System.out.println(i);
-            panel.add(components.get(i));
-        }
-    }
     public static void main(String args[]) throws ParserConfigurationException, SAXException, IOException {
 
 
